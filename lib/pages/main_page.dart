610e4959b9a6bdee60fe66ac39c3117a5e514439
devatom <insertvalue2@gmail.com>
2020-09-07 00:15:36 +0900

    init one;

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter UI Widgets"),
+      ),
+      body: Container(
+          margin: EdgeInsets.all(32),
+          padding: EdgeInsets.all(32),
+          width: 200,
+          height: 200,
+          // color: Colors.green,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.blue,
+                child: Text(
+                  "1",
+                  style: TextStyle(fontSize: 32),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                child: Text(
+                  "2",
+                  style: TextStyle(fontSize: 32),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                child: Text(
+                  "3",
+                  style: TextStyle(fontSize: 32),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(width: 2, color: Colors.red),
+            color: Colors.green,
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Clicked");
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
